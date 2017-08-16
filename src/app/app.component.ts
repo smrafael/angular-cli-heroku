1c640b44926413823a909288518ed49ffdcb0753
Rafael Mendes <rafael.sm1990@gmail.com>
2017-08-16 17:33:13 -0300

    changing test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works! branch test';
+  title = 'app works! branch test again';
 }
